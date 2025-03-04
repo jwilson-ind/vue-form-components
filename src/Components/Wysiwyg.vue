4698175b25d6a47cd1930a31938afab535e25cd9
John Wilson <john@zealouscreations.com>
2025-03-04 08:13:18 -0600

    Major updates derived from being used in various projects

--- src/Components/Wysiwyg.vue
+++ src/Components/Wysiwyg.vue
@@ -1,15 +1,42 @@
 <template>
     <Editor
-        :api-key="apiKey"
+        api-key="no-api-key"
         :init="{
-        plugins: 'lists link image lists table code help wordcount',
-        toolbar: 'undo redo | styles | bold italic numlist bullist | link image '
+        plugins: plugins,
+        toolbar: toolbar,
+        menubar: menubar,
+        height: height,
       }"
     />
 </template>
 
 <script setup>
+/* from https://www.tiny.cloud/docs/tinymce/latest/vite-es6-npm/ */
+import tinymce from 'tinymce';
+import 'tinymce/icons/default/icons.min.js';
+import 'tinymce/themes/silver/theme.min.js';
+import 'tinymce/models/dom/model.min.js';
+import 'tinymce/skins/ui/oxide/skin.js';
+import 'tinymce/plugins/advlist';
+import 'tinymce/plugins/code';
+import 'tinymce/plugins/emoticons';
+import 'tinymce/plugins/emoticons/js/emojis';
+import 'tinymce/plugins/link';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/table';
 import Editor from '@tinymce/tinymce-vue'
-
-const props = defineProps(['apiKey']);
+const props = defineProps({
+    plugins: {
+        default: 'lists link table code help wordcount'
+    },
+    toolbar: {
+        default: 'undo redo | styles | bold italic numlist bullist | link'
+    },
+    menubar: {
+        default: false
+    },
+    height: {
+        default: 500
+    }
+})
 </script>
